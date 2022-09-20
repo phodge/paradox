75628b866496f4e48d816f755209b5fef72a2fcc
Peter Hodge <peter.hodge84@gmail.com>
2022-09-20 23:31:40 +1000

    Unit test for paradox.output.Script

---
+++ tests/test_output_Script.py
@@ -0,0 +1,82 @@
+import io
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from textwrap import dedent
+
+from _paradoxtest import SupportedLang
+from paradox.expressions import PanCall, pan
+
+
+def test_Script(LANG: SupportedLang) -> None:
+    from paradox.output import Script
+
+    s = Script()
+
+    s.add_file_comment("Intro1")
+    s.add_file_comment("Intro2")
+    s.add_file_comment("")
+    s.add_file_comment("Intro3")
+    with s.withCond(pan(True)) as cond:
+        cond.also(PanCall('some_fn'))
+
+    # NOTE: methods from the AcceptsStatements interface are tested elsewhere
+    # in test_interface_AcceptStatements.py
+
+    if LANG == 'php':
+        expected = '''
+            <?php
+
+            // Intro1
+            // Intro2
+            //
+            // Intro3
+
+            if (true) {
+              some_fn();
+            }
+
+        '''
+    elif LANG == 'python':
+        expected = '''
+            """
+            Intro1
+            Intro2
+
+            Intro3
+            """
+            if True:
+              some_fn()
+
+        '''
+    else:
+        assert LANG == 'typescript'
+        expected = '''
+            // Intro1
+            // Intro2
+            //
+            // Intro3
+            if (true) {
+              some_fn();
+            }
+
+        '''
+
+    source = s.get_source_code(lang=LANG, indentstr='  ')
+    assert source == dedent(expected).lstrip()
+
+    fp = io.StringIO()
+    s.write_to_handle(fp, lang=LANG, indentstr='  ')
+    fp.seek(0)
+    stream = fp.read()
+    assert stream == dedent(expected).lstrip()
+
+    with TemporaryDirectory() as tmpdir:
+        ext = {
+                'php': 'php',
+                'python': 'py',
+                'typescript': 'ts',
+        }
+        scriptpath = Path(tmpdir) / f'playscript.{ext}'
+        s.write_to_path(scriptpath, lang=LANG, indentstr='  ')
+        written = scriptpath.read_text()
+        assert written == dedent(expected).lstrip()
